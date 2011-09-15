9fc5cf2adc60de457586c1f11e2cedd1fe90095b
Saverio Trioni <strioni@flumotion.com>
2011-09-15 18:37:21 +0200

    Added pathogen to vim, added zshrc

--- setup.sh
+++ setup.sh
@@ -3,9 +3,23 @@ curl -Lq https://github.com/nvie/gitflow/raw/develop/contrib/gitflow-installer.s
 curl -Lq https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 curl -Lqs https://rvm.beginrescueend.com/install/rvm | sh
 # install vim plugins
-mkdir -p ~/git
-cd ~/git
-git clone git://github.com/msanders/snipmate.vim.git && (cd snipmate.vim && cp -R * ~/.vim)
-git clone https://github.com/scrooloose/nerdtree.git && (cd nerdtree && cp -R * ~/.vim)
-git clone https://github.com/tpope/vim-rails.git && (cd vim-rails && cp -R * ~/.vim)
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim > ~/.vim/autoload/pathogen.vim
+grep "call pathogen#infect()" ~/.vimrc || (echo "call pathogen#infect()" >> ~/.vimrc)
+cd ~/.vim/bundle
+if [[ -d snipmate.vim ]]; then
+    cd snipmate.vim; git pull; cd -
+else
+    git clone git://github.com/msanders/snipmate.vim.git
+fi
+if [[ -d nerdtree ]]; then
+    cd nerdtree; git pull; cd -
+else
+    git clone https://github.com/scrooloose/nerdtree.git
+fi
+if [[ -d vim-rails ]]; then
+    cd vim-rails; git pull; cd -
+else
+    git clone https://github.com/tpope/vim-rails.git
+fi
 
