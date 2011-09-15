57210a7ee1ff2b171db5e1b1692983579e7712d7
Saverio Trioni <strioni@flumotion.com>
2011-09-15 17:52:34 +0200

    Added vim plugin installation

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+# install things
 curl -Lq https://github.com/nvie/gitflow/raw/develop/contrib/gitflow-installer.sh | sudo sh
 curl -Lq https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+curl -Lqs https://rvm.beginrescueend.com/install/rvm | sh
+# install vim plugins
+mkdir -p ~/git
+cd ~/git
+git clone git://github.com/msanders/snipmate.vim.git && (cd snipmate.vim && cp -R * ~/.vim)
+git clone https://github.com/scrooloose/nerdtree.git && (cd nerdtree && cp -R * ~/.vim)
+git clone https://github.com/tpope/vim-rails.git && (cd vim-rails && cp -R * ~/.vim)
 
